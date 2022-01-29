eb17e49f2bdc2d27b5fbcb626962d57fda9db0ad
Devam Savjani <devamsavjani@rocketmail.com>
2022-01-29 21:00:12 +0000

    Start lab2

--- Lab2/flooding/lib/flooding.ex
+++ Lab2/flooding/lib/flooding.ex
@@ -6,18 +6,23 @@
 
 defmodule Flooding do
 
-def start do 
+def start do
   config = Helper.node_init()
-  start(config, config.start_function) 
+  start(config, config.start_function)
 end # start/0
 
 defp start(_,      :cluster_wait), do: :skip
 defp start(config, :cluster_start) do
   IO.puts "-> Flooding at #{Helper.node_string()}"
 
+  peers = Enum.map(0..9, fn(n) ->
+    Node.spawn(:'peer#{n}_#{config.node_suffix}', Peer, :start, [])
+  end)
+
+  # Enum.each(peers, fn x -> IO.puts(inspect(x)) end)
+
   # add your code here
 
 end # start/2
 
 end # Flooding
-
---
+++ Lab2/flooding/lib/peer.ex
@@ -0,0 +1,21 @@
+
+# distributed algorithms, n.dulay, 10 jan 22
+# basic flooding, v1
+
+defmodule Peer do
+
+  # add your code here, start(), next() and any other functions
+
+  def start do
+    IO.puts("Starting Peer")
+    # receive do
+    #   {:bind, peers} -> next(peers)
+    #     # code
+    # end
+  end
+
+  def next(peers) do
+    IO.puts(peers)
+  end
+
+end # Peer
